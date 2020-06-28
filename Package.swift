78c3ec42c6d826fd1db98ecb72f7bc0a8a3f953b
Kirill Titov <work@kirilltitov.com>
2020-06-28 11:26:12 +0300

    Renamed package from LGNKit-Swift to LGNKit in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "LGNKit-Swift",
+    name: "LGNKit",
     platforms: [.macOS(.v10_15)],
     products: [
         .library(name: "LGNCore", targets: ["LGNCore"]),
