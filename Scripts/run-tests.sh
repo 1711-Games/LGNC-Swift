3630bcc1c4baa337753292aaae132ca40c6eeba8
Nikolay Belikov <nb@nbelikov.com>
2020-09-25 14:00:03 +0300

    Scripts/run-tests.sh will manage package tests too
    * Remove generated files for package tests from version control.  They
      will be regenerated by the script.  As a downside, `swift test` will
      no longer work on freshly checked out repository.
    * Rename `Tests/LGNCSwiftTests/Generated` to
      `Tests/LGNCSwiftTests/Codegen` for consistency with integration tests.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -4,9 +4,13 @@ set -eu
 
 root_dir="$(cd "$(dirname "$0")/.." && pwd)"
 integration_dir="$root_dir/IntegrationTests"
+integration_codegen_dir="$integration_dir/Sources/IntegrationTests/Codegen"
+tests_dir="$root_dir/Tests/LGNCSwiftTests"
+tests_codegen_dir="$tests_dir/Codegen"
 build_dir="$root_dir/.build"
 LGNBuilder_dir="${integration_dir}/Var/LGNBuilder"
 LGNBuilder_repository="git@github.com:1711-Games/LGNBuilder.git"
+LGNBuilder_executable="$LGNBuilder_dir/Scripts/generate"
 
 if [ -d "$LGNBuilder_dir" ]; then
     (cd "$LGNBuilder_dir" && git pull)
@@ -15,12 +19,16 @@ else
     git clone "$LGNBuilder_repository" "$LGNBuilder_dir"
 fi
 
-"$LGNBuilder_dir/Scripts/generate" \
-    --lang   Swift \
-    --input  "$LGNBuilder_dir/IntegrationTests/Schema" \
-    --output "$integration_dir/Sources/IntegrationTests/Codegen"
-
+mkdir -p "$tests_codegen_dir"
+"$LGNBuilder_executable" --lang Swift \
+    --input  "$tests_dir/Schema" \
+    --output "$tests_codegen_dir"
 swift test --package-path "$root_dir"
+
+mkdir -p "$integration_codegen_dir"
+"$LGNBuilder_executable" --lang Swift \
+    --input  "$LGNBuilder_dir/IntegrationTests/Schema" \
+    --output "$integration_codegen_dir"
 swift run  --package-path "$integration_dir" --build-path "$build_dir" \
     IntegrationTests --tests-directory "$LGNBuilder_dir/IntegrationTests/Tests"
 
